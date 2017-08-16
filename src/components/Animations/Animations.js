5c9751fe263ee696891ea3210fceee6175b543b4
Daniel Yuschick <danyuschick@gmail.com>
2017-08-16 14:34:33 +0300

    essentially stashing to do more work on master

--- src/components/Animations/Animations.js
+++ src/components/Animations/Animations.js
@@ -8,12 +8,43 @@ class Animations extends Component {
     super();
 
     this.getHeaderAnimation = this.getHeaderAnimation.bind(this);
+    this.buildWatchsettings = this.buildWatchsettings.bind(this);
+  }
+
+  componentDidMount() {
+    this.SnapFoo = snapFoo(`#${this.props.parent}`);
+    this.buildWatchsettings();
+  }
+
+  buildWatchsettings() {
+    let settings = null;
+    let element;
+    let finalbuild;
+
+    switch (this.props.type) {
+      case 'header':
+        element = '#ball-one';
+        settings = {
+          duration: 2500,
+          loop: true,
+          path: '#path-one'
+        };
+        console.log(snapFoo);
+        break;
+      default:
+        return false;
+    }
+
+    return finalbuild;
   }
 
   getHeaderAnimation() {
     return (
       <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 510 510" xmlSpace="preserve">
-        <path id="path-four" className="header-st0" d="M466 254.5H34c0 0-41 208 208 208v-431C242 31.5 466 7.5 466 254.5zM256.6 29.4v432c0 0 208 41 208-208h-431C33.6 253.4 9.6 29.4 256.6 29.4zM240.9 29.4v432c0 0-208 41-208-208h431C463.9 253.4 487.9 29.4 240.9 29.4zM31.5 254.5h432c0 0 41 208-208 208v-431C255.5 31.5 31.5 7.5 31.5 254.5z"/>
+        <path id="path-four" className="header-st0" d="M466 254.5H34c0 0-41 208 208 208v-431C242 31.5 466 7.5 466 254.5z"/>
+        <path id="path-three" className="header-st0" d="M256.6 29.4v432c0 0 208 41 208-208h-431C33.6 253.4 9.6 29.4 256.6 29.4z"/>
+        <path id="path-two" className="header-st0" d="M240.9 29.4v432c0 0-208 41-208-208h431C463.9 253.4 487.9 29.4 240.9 29.4z"/>
+        <path id="path-one" className="header-st0" d="M31.5 254.5h432c0 0 41 208-208 208v-431C255.5 31.5 31.5 7.5 31.5 254.5z"/>
         <circle id="ball-one" className="header-st1" cx="31.5" cy="255" r="26.5"/>
         <circle id="ball-two" className="header-st1" cx="255" cy="31.5" r="26.5"/>
         <circle id="ball-three" className="header-st1" cx="255" cy="31.5" r="26.5"/>
