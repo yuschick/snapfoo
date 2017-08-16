04af7eb4193ed2ee06f03cfacf8bb05ee8125578
Daniel Yuschick <danyuschick@gmail.com>
2017-08-16 15:12:35 +0300

    working

--- src/components/Animations/Animations.js
+++ src/components/Animations/Animations.js
@@ -21,10 +21,10 @@ class Animations extends Component {
     let element;
     let type;
     let callback = null;
-    this.SnapFoo = snapFoo(`#${this.props.parent}`);
 
     switch (this.props.type) {
       case 'header':
+        this.SnapFoo = snapFoo(`#header-animation`);
         type = 'animatePath';
         element = '#ball-one';
         settings = {
@@ -43,7 +43,7 @@ class Animations extends Component {
 
   getHeaderAnimation() {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 510 510" xmlSpace="preserve">
+      <svg xmlns="http://www.w3.org/2000/svg" id='header-animation' version="1.1" x="0" y="0" viewBox="0 0 510 510" xmlSpace="preserve">
         <path id="path-four" className="header-st0" d="M466 254.5H34c0 0-41 208 208 208v-431C242 31.5 466 7.5 466 254.5z"/>
         <path id="path-three" className="header-st0" d="M256.6 29.4v432c0 0 208 41 208-208h-431C33.6 253.4 9.6 29.4 256.6 29.4z"/>
         <path id="path-two" className="header-st0" d="M240.9 29.4v432c0 0-208 41-208-208h431C463.9 253.4 487.9 29.4 240.9 29.4z"/>
